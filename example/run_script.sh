9432dadba4fff52dd378b8df7a7d887f56eb1725
Gregor Gorjanc <gregor.gorjanc@gmail.com>
2025-12-19 10:09:11 +0100

    Revise run_script.sh
    Updated comments and mkdir use.

--- example/run_script.sh
+++ example/run_script.sh
@@ -1,19 +1,23 @@
-mkdir outputs
 # AlphaImpute2 is a command line package for imputation in pedigree populations
 # Install AlphaImpute2 via pip using:
-# pip install <wheel name>
+# pip install AlphaImpute2
 
-#To check command line arguments run AlphaImpute2 without any arguments.
-AlphaImpute2
+# To see command line arguments run AlphaImpute2 without any arguments or providing -h or --help
+# AlphaImpute2
+# AlphaImpute2 -h
+# AlphaImpute2 --help
 
-# Example 1: Run the full algorithm which runs both population and pedigree based imputation.
+# Below is a set of examples that give you a flavour on how to run and use AlphaImpute2
+mkdir -p outputs
+
+# Example 1: Run the full algorithm with population and pedigree based imputation
 
 AlphaImpute2 -genotypes data/genotypes.txt \
              -pedigree data/pedigree.txt \
              -out outputs/ai2 \
              -maxthreads 4
 
-# Example 1b: Run the population imputation algorithm only (used when pedigree data is unavailible).
+# Example 1b: Run the population imputation algorithm only (used when pedigree data is unavailible)
 
 AlphaImpute2 -genotypes data/genotypes.txt \
              -pedigree data/pedigree.txt \
@@ -21,7 +25,7 @@ AlphaImpute2 -genotypes data/genotypes.txt \
              -pop_only \
              -maxthreads 4
 
-# Example 2: Run the pedigree based imputation algorithm only to get high-confidence phased haplotypes.
+# Example 2: Run the pedigree based imputation algorithm and only call high-confident haplotypes
 
 AlphaImpute2 -genotypes data/genotypes.txt \
              -pedigree data/pedigree.txt \
